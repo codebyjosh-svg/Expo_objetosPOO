c01941d18ce80d908d337fa80d353c25708076ef
Joshuasuy <jsuy-2026083@kinal.edu.gt>
2026-04-23 20:11:55 -0600

    003 edite Animal y agregue a main

--- Expo_objetosPOO/src/POO/clases/Animal.java
+++ Expo_objetosPOO/src/POO/clases/Animal.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package poo.clases;
 
-/**
- *
- * @author Darwin
- */
 public class Animal {
-    
+    public class Animal{
+        Systme.out.println("Sonido de animal");
+    }
 }
